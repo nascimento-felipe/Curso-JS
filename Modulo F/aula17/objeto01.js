2f86923ffe0e352153aedb91766c27fcb86f438a
Felipe de Andrade Nascimento <nascimentofelipe565@gmail.com>
2020-04-06 16:40:12 -0300

    Última pasta adicionada

---
+++ Modulo F/aula17/objeto01.js
@@ -0,0 +1,12 @@
+//POO em JS.
+
+let amigo = {
+    nome: 'José',
+    sexo: 'M',
+    peso: 85.4,
+    engordar(p = 0) {
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+console.log(amigo.nome)
